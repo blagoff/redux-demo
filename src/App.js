b30efd7c2429248efda5664c48930bdf9a636005
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-03-21 23:37:42 +0100

    finish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CommentsListContainer from './CommentsListContainer';
+
+const App = () => {
+  return (
+    <div className="comment">
+      <CommentsListContainer />
+    </div>
+  );
+};
+
+export default App;
